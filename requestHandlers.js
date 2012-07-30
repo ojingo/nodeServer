0d3491dfa494ce9fa12644c79536cf56e4bb84fc
TJ <tj@ojingolabs.com>
2012-07-30 22:15:09 +0400

    clean ups

--- requestHandlers.js
+++ requestHandlers.js
@@ -9,14 +9,14 @@ function startpage(response) {
   console.log("Request handler 'start' was called.");
   // now we use a response with the non block but also can come back when done?
   exec("ls -lah", function (error, stdout, stderr) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+  	response.writeHead(200, {"Content-Type": "text/plain"});
     response.write(stdout);
     response.end();
   });
 }
 
 // a upload URL request...
-function upload() {
+function upload(response) {
 	console.log("Request handler 'upload' was called.");
 	// 
 	response.writeHead(200, {"Content-Type": "text/plain"});
@@ -24,9 +24,12 @@ function upload() {
 	response.end();
 }
 
-function test() {
+function test(response) {
 	console.log("Testing handler 'test' was called.");
-	return "Testing";
+	// 
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Test!");
+	response.end();
 }
 
 // exporting these functions so they are available to the others who require this handler.js
