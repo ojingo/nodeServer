f4031c42a4e171291ec28fe7325776cc2f31b6d7
TJ <tj@ojingolabs.com>
2012-07-31 23:13:19 +0400

    parse post data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 // the request handlers!
 // now add require for a new node module called child_process that executes a shell from within this process
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
 // start URL request...
 function startpage(response) {
@@ -33,7 +34,8 @@ console.log("Request handler 'start' was called.");
 function upload(response, postData) {
   console.log("Request handler 'upload' was called.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("You've sent: " + postData);
+  response.write("You've sent the text: "+
+  querystring.parse(postData).text);
   response.end();
 }
 
